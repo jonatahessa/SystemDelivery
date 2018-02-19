e55459786cf6ff2c5f58fa73251a438b5888961c
jonatahessa <jonata.hessa@gmail.com>
2018-02-19 11:55:39 -0300

    First Commit

---
+++ src/main/java/br/com/systemdelivery/systemdelivery/SystemdeliveryApplication.java
@@ -0,0 +1,12 @@
+package br.com.systemdelivery.systemdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemdeliveryApplication.class, args);
+	}
+}
